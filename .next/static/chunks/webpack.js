8c81f8815d29906061af6747a633c76c3e26e696
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2025-01-10 21:38:22 +0000

    add  endpoint

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a40115cdcf909f3e"; }
+/******/ 		__webpack_require__.h = function() { return "b21c62bac055144d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- models/calculadora.js
+++
@@ -1,8 +0,0 @@
-function somar(numero1, numero2) {
-  if (isNaN(numero1) || isNaN(numero2)) {
-    return "Erro";
-  }
-  return numero1 + numero2;
-}
-
-exports.somar = somar;
