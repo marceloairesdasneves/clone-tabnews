408373ab03c0ed3c76febf97b3ff9d9d28bd083e
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2025-01-29 00:22:40 +0000

    "`Teste analise de erro`"

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,10 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
-  res.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+  res.status(200).json({
+    updated_at: updatedAt,
+  });
 }
 
 export default status;
