b2560103e49dfdfdf253e31ab499e7b0041036c8
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2025-02-11 01:09:35 +0000

    test10022

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,9 @@
 import database from "infra/database.js";
 
-async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sun");
-  console.log(result.rows);
-  res.status(200).json({chave: "sao acima da media"});
+async function status(request, response) {
+  const updateAt = new Date().toISOString();
+  response.status(200).json({
+    update_at: updateAt,
+  });
 }
 export default status;
