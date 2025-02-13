087587b0d953ca802756915b5d0d94a36a71a936
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2025-02-13 22:58:59 +0000

    test

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,22 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     version: process.env.POSTGRES_VERSION,
   });
-  await client.connect();
-
+  console.log("Credenciais do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    version: process.env.POSTGRES_VERSION,
+  })
+  
   try{
+    await client.connect();
     const result = await client.query(queryObject);
       return result;
   }catch (error){
     console.error(error);
+    throw error;
   }finally {
     await client.end();
   }
